77cfef1e28df61b8a9e90af5358493e3c1f6ece3
nachos836 <nachos836@gmail.com>
2019-11-15 14:39:34 +0200

    small bugfix with MacOS build v1

--- configs/MODIFIERS.mk
+++ configs/MODIFIERS.mk
@@ -17,31 +17,34 @@
 # Konsole clear routine - clear && echo -en "\e[3J"
 # Otherwise try "reset" or use whatever u want
 #
+
+FRAMEWORKS_DIR	:=	$(CURDIR)/frameworks
+
 ifeq ($(OS_DETECT),$(OS_LINUX))
 	CLEAR = clear && printf "\e[3J"
 # 	COMPILE_APP_RULE	+=	-lX11 -pthread -lm
 	COMPILE_APP_RULE	+=	-lX11 -pthread -lm -lSDL2 -lSDL2_image -lSDL2_ttf
 # 	COMPILE_APP_RULE	+=	-fopenmp
 # 	COMPILE_OBJ_RULE	+=	-fopenmp
-# 	COMPILE_APP_RULE	+=	-mavx -mavx2 -mfma -mprefetchwt1 -mprfchw \
-# 							-mno-sse \
-# 							-mno-sse2 \
-# 							-mno-sse3 \
-# 							-mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a \
-# 							-mno-ssse3
-# 	COMPILE_OBJ_RULE	+=	-mavx -mavx2 -mfma -mprefetchwt1 -mprfchw \
-# 							-mno-sse \
-# 							-mno-sse2 \
-# 							-mno-sse3 \
-# 							-mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a \
-# 							-mno-ssse3
+	COMPILE_APP_RULE	+=	-mavx -mavx2 -mfma -mprefetchwt1 -mprfchw \
+							-mno-sse \
+							-mno-sse2 \
+							-mno-sse3 \
+							-mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a \
+							-mno-ssse3
+	COMPILE_OBJ_RULE	+=	-mavx -mavx2 -mfma -mprefetchwt1 -mprfchw \
+							-mno-sse \
+							-mno-sse2 \
+							-mno-sse3 \
+							-mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-sse4a \
+							-mno-ssse3
 
 
 
 
 else ifeq ($(OS_DETECT),$(OS_OSX))
 	CLEAR = clear && printf "\e[3J"
-	COMPILE_APP_RULE	+=	-framework MetalKit -framework Metal -framework cocoa
+# 	COMPILE_APP_RULE	+=	-framework MetalKit -framework Metal -framework cocoa
 	
 	COMPILE_OBJ_RULE	+=	-I $(FRAMEWORKS_DIR)/SDL2.framework/Headers \
 							-I $(FRAMEWORKS_DIR)/SDL2_image.framework/Headers \
--- libraries/libftsdl/configs/MODIFIERS.mk
+++ libraries/libftsdl/configs/MODIFIERS.mk
@@ -18,28 +18,10 @@
 # Otherwise try "reset" or use whatever u want
 #
 
-FRAMEWORKS_DIR	:=	$(CURDIR)/frameworks
-$(info $(FRAMEWORKS_DIR))
-
 ifeq ($(OS_DETECT),$(OS_LINUX))
 	CLEAR = clear && printf "\e[3J"
-	COMPILE_APP_RULE	+=	-lX11 -pthread -lm -lSDL2 -lSDL2_image -lSDL2_ttf
 else ifeq ($(OS_DETECT),$(OS_OSX))
 	CLEAR = clear && printf "\e[3J"
-
-	COMPILE_OBJ_RULE	+=	-I $(FRAMEWORKS_DIR)/SDL2.framework/Headers \
-							-I $(FRAMEWORKS_DIR)/SDL2_image.framework/Headers \
-							-I $(FRAMEWORKS_DIR)/SDL2_ttf.framework/Headers \
-							-F $(FRAMEWORKS_DIR)
-
-	COMPILE_APP_RULE	+=	-rpath $(FRAMEWORKS_DIR) \
-							-framework SDL2 \
-							-framework SDL2_image \
-							-framework SDL2_ttf \
-							-framework OpenGL \
-							-framework AppKit
-
 else ifeq ($(OS_DETECT),$(OS_WINDOWS))
 	CLEAR = clean"
-	COMPILE_APP_RULE	+=	-lgdi32
 endif
