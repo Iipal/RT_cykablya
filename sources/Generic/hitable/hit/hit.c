927d8cafd749569bd953e7334764af5f58acaa48
Sergey SHEVCHUK <sshevchu@e1r10p9.unit.ua>
2019-11-17 01:16:48 +0200

    gitignore && hit

--- sources/Generic/hitable/hit/hit.c
+++ sources/Generic/hitable/hit/hit.c
@@ -1,15 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hit.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshevchu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/17 01:08:03 by sshevchu          #+#    #+#             */
+/*   Updated: 2019/11/17 01:08:07 by sshevchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #if !defined(IMPLEMETNATION) && !defined(DECLARATION)
 # define IMPLEMETNATION
 # define DECLARATION
 # include "hit.h"
 #endif
 
-# include "material.h"
-
 static inline _Bool __attribute__((CONST,CLONE,ARCH))
 	hit_condition(register const t_record_sf record)
 {
-	return ((_Bool)((t_record_mask_qi)record)[0]);
+	return (!!((t_record_mask_qi)record)[0]);
 }
 
 static inline float __attribute__((CONST,CLONE,ARCH))
@@ -18,42 +28,39 @@ static inline float __attribute__((CONST,CLONE,ARCH))
 	return ((record)[1]);
 }
 
-
-
-struct	s_hlp
+static char __attribute__((CONST,ARCH))
+	type_select(register const union u_hitables hitables)
 {
-	const union u_hitables	*restrict	hitables;
-	const union u_record_helper			prev_record;
-	const t_ray_sf						ray;
-	const t_v2sf						time;
-	const size_t						i;
-};
-
-static t_record_sf __attribute__((CONST,CLONE,ARCH))
-	hit_sphere(const struct s_hlp s)
-{
-	register union u_record_helper	currect_record;
-
-	currect_record.sf = hit(*(s.hitables[s.i].sphere.self),
-		s.ray, s.time, s.prev_record.sf);
-	if (hit_condition(currect_record.sf)
-	&& hit_distance(currect_record.sf) < s.time.y)
-	{
-		currect_record.qi[1] =
-		(char)(type(*(s.hitables[s.i].sphere.material)));
-		currect_record.hi[1] = (short)(s.i);
-	}
-	return (currect_record.sf);
+	return ((char[]){ 0,
+			(char)(type(*(hitables.sphere.material))),
+			(char)(type(*(hitables.cone.material))),
+			(char)(type(*(hitables.cylinder.material))),
+			(char)(type(*(hitables.tetrahedron.material))),
+			(char)(type(*(hitables.plane.material))), 0
+		}[hitables.generic.type]);
 }
 
-
-
-
-
-
+static t_record_sf __attribute__((CONST,ARCH))
+	record_select(register const union u_hitables hitables,
+				register const t_ray_sf ray,
+				register const t_v2sf time,
+				register const t_record_sf currect_record)
+{
+	if (hitables.generic.type == SPHERE)
+		return (hit(*(hitables.sphere.self), ray, time, currect_record));
+	else if (hitables.generic.type == CONE)
+		return (hit(*(hitables.cone.self), ray, time, currect_record));
+	else if (hitables.generic.type == CYLINDER)
+		return (hit(*(hitables.cylinder.self), ray, time, currect_record));
+	else if (hitables.generic.type == TETRAHEDRON)
+		return (hit(*(hitables.tetrahedron.self), ray, time, currect_record));
+	else if (hitables.generic.type == PLANE)
+		return (hit(*(hitables.plane.self), ray, time, currect_record));
+	return (0);
+}
 
 t_record_sf __attribute__((CONST,CLONE,ARCH))
-	hit(register const union u_hitables * restrict hitables,
+	hit(register const union u_hitables *restrict hitables,
 		register const t_ray_sf ray,
 		register const float t_min,
 		register const float t_max)
@@ -63,76 +70,20 @@ t_record_sf __attribute__((CONST,CLONE,ARCH))
 	register size_t					i;
 	register float					closest_so_far;
 
-	currect_record.sf = __extension__((typeof(currect_record.sf)){});
-	return_candidate = __extension__((typeof(return_candidate)){});
+	currect_record.sf = (const t_record_sf){ 0 };
+	return_candidate = (const t_record_sf){ 0 };
 	closest_so_far = t_max;
 	i = ~0UL;
 	while (++i < hitables->generic.count)
-		// if ((hitables[i].generic.type == SPHERE) && (hit_condition(currect_record.sf = hit_sphere((const struct s_hlp){
-		// hitables, currect_record, ray, vec(t_min, closest_so_far), i }))))
-		// 	return_candidate = currect_record.sf;
-
-		if (hitables[i].generic.type == SPHERE)
+		if (hitables[i].generic.type != MATERIAL_START)
 		{
-			currect_record.sf = hit(*(hitables[i].sphere.self),
-					ray, vec(t_min, closest_so_far), currect_record.sf);
+			currect_record.sf = record_select(hitables[i], ray,
+				vec(t_min, closest_so_far), currect_record.sf);
 			if (hit_condition(currect_record.sf)
 			&& hit_distance(currect_record.sf) < closest_so_far)
 			{
 				closest_so_far = hit_distance(currect_record.sf);
-				currect_record.qi[1] = (char)(type(*(hitables[i].sphere.material)));
-				currect_record.hi[1] = (short)(i);
-				return_candidate = currect_record.sf;
-			}
-		}
-		else if (hitables[i].generic.type == CONE)
-		{
-			currect_record.sf = hit(*(hitables[i].cone.self),
-					ray, vec(t_min, closest_so_far), currect_record.sf);
-			if (hit_condition(currect_record.sf)
-			&& hit_distance(currect_record.sf) < closest_so_far)
-			{
-				closest_so_far = hit_distance(currect_record.sf);
-				currect_record.qi[1] = (char)(type(*(hitables[i].cone.material)));
-				currect_record.hi[1] = (short)(i);
-				return_candidate = currect_record.sf;
-			}
-		}
-		else if (hitables[i].generic.type == CYLINDER)
-		{
-			currect_record.sf = hit(*(hitables[i].cylinder.self),
-					ray, vec(t_min, closest_so_far), currect_record.sf);
-			if (hit_condition(currect_record.sf)
-			&& hit_distance(currect_record.sf) < closest_so_far)
-			{
-				closest_so_far = hit_distance(currect_record.sf);
-				currect_record.qi[1] = (char)(type(*(hitables[i].cylinder.material)));
-				currect_record.hi[1] = (short)(i);
-				return_candidate = currect_record.sf;
-			}
-		}
-		else if (hitables[i].generic.type == TETRAHEDRON)
-		{
-			currect_record.sf = hit(*(hitables[i].tetrahedron.self),
-					ray, vec(t_min, closest_so_far), currect_record.sf);
-			if (hit_condition(currect_record.sf)
-			&& hit_distance(currect_record.sf) < closest_so_far)
-			{
-				closest_so_far = hit_distance(currect_record.sf);
-				currect_record.qi[1] = (char)(type(*(hitables[i].tetrahedron.material)));
-				currect_record.hi[1] = (short)(i);
-				return_candidate = currect_record.sf;
-			}
-		}
-		else if (hitables[i].generic.type == PLANE)
-		{
-			currect_record.sf = hit(*(hitables[i].plane.self),
-					ray, vec(t_min, closest_so_far), currect_record.sf);
-			if (hit_condition(currect_record.sf)
-			&& hit_distance(currect_record.sf) < closest_so_far)
-			{
-				closest_so_far = hit_distance(currect_record.sf);
-				currect_record.qi[1] = (char)(type(*(hitables[i].plane.material)));
+				currect_record.qi[1] = type_select(hitables[i]);
 				currect_record.hi[1] = (short)(i);
 				return_candidate = currect_record.sf;
 			}
